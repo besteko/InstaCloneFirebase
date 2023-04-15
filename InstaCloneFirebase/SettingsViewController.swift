a8046a90e6db95860445c4f67822294cd78de370
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-04-16 00:55:02 +0300

    first commit

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Beste Kocaoglu on 12.04.2023.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutClicked(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+           self.performSegue(withIdentifier: "toViewController", sender: nil)
+        }catch{
+            print("error")
+        }
+    }
+    
+    
+
+}
---
+++ InstaCloneFirebase/UploadViewController.swift
@@ -0,0 +1,100 @@
+//
+//  UploadViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Beste Kocaoglu on 12.04.2023.
+//
+
+import UIKit
+import Firebase
+
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+       
+    }
+    
+    @objc func chooseImage(){
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController,animated: true,completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func makeALert(titleInput: String,messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    
+    
+    @IBAction func actionButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data) { metadata, error in
+                if error != nil{
+                    self.makeALert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    
+                    imageReference.downloadURL { url, error in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            
+                          //DATABASE
+                            
+                            let firestoredatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!,"postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : FieldValue.serverTimestamp() , "likes" : 0]
+                            
+                            firestoreReference = firestoredatabase.collection("Posts").addDocument(data: firestorePost ,completion: {(error) in
+                                if error != nil {
+                                    self.makeALert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                }else {
+                                    
+                                    self.imageView.image = UIImage(named: "taptoselect.png")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
+                                    
+                                }
+                            })
+                            
+                             
+                        }
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+}
